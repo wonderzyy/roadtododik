3314f7c50039f2a94f09b0f9f318cddfb1e54519
wonderzyy <danya.busha31@gmail.com>
2023-09-27 15:33:17 +0200

    update

--- karel.c
+++ karel.c
@@ -1,22 +1,36 @@
-  1 #include <karel.h>
-  2 
-  3 
-  4 bool front_is_blocked();
-  5 
-  6 int main() {
-  7    turn_on("stairs1.kw");
-  8 
-  9 sa]]
- 10 
- 11 ····
- 12    
- 13    pick_beeper();
- 14    while (front_is_blocked()) {
- 15       turn_left();
- 16       step();
- 17       turn_right();
- 18    step();
- 19    }
- 20    
- 21    turn_off();
- 22 }
+#include <karel.h>
+
+void turn_right();
+bool front_is_blocked();
+
+int main() {
+   turn_on("stairs1.kw");
+
+   set_step_delay(400);
+
+   pick_beeper();
+   while (front_is_blocked()) {
+      turn_left();
+      step();
+      turn_right();
+      step();
+   }
+
+   turn_off();
+}
+
+bool front_is_blocked() {
+    if (front_is_clear()) {
+        return false;
+    }
+    return true;
+    // return !front_is_clear();
+}
+
+void turn_right() {
+   set_step_delay(0);
+   turn_left();
+   turn_left();
+   set_step_delay(400);
+   turn_left();
+}
